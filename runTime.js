f0312cf91ecb6f56beec715fc11101f243d2909e
Soumya Khanna <soumya.khanna99@gmail.com>
2020-04-10 12:56:33 +0300

    Add files via upload

---
+++ runTime.js
@@ -0,0 +1,26 @@
+var theme = document.querySelector(".theme");
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+})
+
+$(".calendar").click(function(){
+  window.location = "calendar.html"
+})
+
+$(".plan").click(function(){
+  window.location = "plan.html"
+})
+
+$(".social").click(function(){
+  window.location = "social.html"
+})
+
+$(".feelingdown").click(function(){
+  window.location = "feelingdown.html"
+})
+
+$(".home").click(function(){
+  window.location = "index.html"
+})
+
